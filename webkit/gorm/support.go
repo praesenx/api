76abdb6c45004d31fa47d696bce5966c735901c5
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-04-16 11:08:16 +0800

    style

--- webkit/gorm/support.go
+++ webkit/gorm/support.go
@@ -1,18 +1,18 @@
 package gorm
 
 import (
-    "errors"
-    "gorm.io/gorm"
+	"errors"
+	"gorm.io/gorm"
 )
 
 func IsNotFound(err error) bool {
-    return err != nil && errors.Is(err, gorm.ErrRecordNotFound)
+	return err != nil && errors.Is(err, gorm.ErrRecordNotFound)
 }
 
 func IsFoundButHasErrors(err error) bool {
-    return err != nil && !errors.Is(err, gorm.ErrRecordNotFound)
+	return err != nil && !errors.Is(err, gorm.ErrRecordNotFound)
 }
 
 func HasDbIssues(err error) bool {
-    return IsNotFound(err) || IsFoundButHasErrors(err)
+	return IsNotFound(err) || IsFoundButHasErrors(err)
 }
