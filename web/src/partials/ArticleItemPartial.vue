a82231b2a9487d61d93e5790f7870c500dcdd3bc
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-05-30 17:07:13 +0800

    home page

--- web/src/partials/ArticleItemPartial.vue
+++ web/src/partials/ArticleItemPartial.vue
@@ -3,10 +3,10 @@
 		<div class="flex items-start">
 			<img class="rounded-sm w-16 h-16 sm:w-[88px] sm:h-[88px] object-cover mr-6" :src="item.image" width="88" height="88" :alt="item.title" />
 			<div>
-				<div class="text-xs text-slate-700 uppercase mb-1 dark:text-slate-500"><span class="dark:text-fuchsia-400 text-fuchsia-500">—</span>&nbsp;{{ item.date }}</div>
+				<div class="text-xs text-slate-700 uppercase mb-1 dark:text-slate-500">{{ item.date }}</div>
 				<h3 class="text-slate-700 font-aspekta text-lg font-[650] mb-1 dark:text-slate-300">
 					<router-link
-						class="inline-flex relative hover:text-fuchsia-500 duration-150 ease-out before:scale-x-0 before:origin-center before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:translate-y-1/4 before:-rotate-2 hover:before:scale-100 before:duration-150 before:ease-in-out"
+						class="inline-flex relative hover:text-fuchsia-500 dark:hover:text-teal-500 duration-150 ease-out before:scale-x-0 before:origin-center before:absolute before:inset-0 before:bg-sky-200 dark:before:bg-sky-500 before:opacity-30 before:-z-10 before:translate-y-1/4 before:-rotate-2 hover:before:scale-100 before:duration-150 before:ease-in-out"
 						:to="item.slug"
 					>
 						{{ item.title }}
@@ -16,7 +16,7 @@
 					<div class="grow text-sm text-slate-500 dark:text-slate-600">
 						{{ item.excerpt }}
 					</div>
-					<router-link class="hidden lg:flex shrink-0 text-sky-500 dark:text-fuchsia-400 items-center justify-center w-12 group" :to="item.slug" tabindex="-1">
+					<router-link class="hidden lg:flex shrink-0 text-fuchsia-500 dark:text-teal-500 items-center justify-center w-12 group" :to="item.slug" tabindex="-1">
 						<svg class="fill-current group-hover:translate-x-2 duration-150 ease-in-out" xmlns="http://www.w3.org/2000/svg" width="14" height="12">
 							<path d="M9.586 5 6.293 1.707 7.707.293 13.414 6l-5.707 5.707-1.414-1.414L9.586 7H0V5h9.586Z" />
 						</svg>
