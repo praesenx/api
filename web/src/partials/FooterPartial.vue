e48c8ed0264e90a2d62ebdc63e7aebf0ee9a5f0a
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-06 14:27:17 +0800

    chore: remove web dir

--- web/src/partials/FooterPartial.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<footer class="border-t border-slate-200 dark:border-slate-800">
-		<div class="py-8">
-			<div class="text-center md:flex md:items-center md:justify-between">
-				<div class="text-sm text-slate-500 dark:text-slate-400">Copyright &copy; gocanto. All rights reserved.</div>
-			</div>
-		</div>
-	</footer>
-</template>
