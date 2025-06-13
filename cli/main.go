d6bc04571b64bca31313be2a3cc6f07be7005d36
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-13 16:02:01 +0800

    format

--- cli/main.go
+++ cli/main.go
@@ -1,58 +1,58 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "github.com/oullin/cli/menu"
-    "github.com/oullin/pkg/cli"
-    "os"
-    "time"
+	"bufio"
+	"fmt"
+	"github.com/oullin/cli/menu"
+	"github.com/oullin/pkg/cli"
+	"os"
+	"time"
 )
 
 func main() {
-    panel := menu.Panel{
-        Reader: bufio.NewReader(os.Stdin),
-    }
+	panel := menu.Panel{
+		Reader: bufio.NewReader(os.Stdin),
+	}
 
-    panel.PrintMenu()
+	panel.PrintMenu()
 
-    for {
-        err := panel.CaptureInput()
+	for {
+		err := panel.CaptureInput()
 
-        if err != nil {
-            cli.MakeTextColour(err.Error(), cli.Red).Println()
-            continue
-        }
+		if err != nil {
+			cli.MakeTextColour(err.Error(), cli.Red).Println()
+			continue
+		}
 
-        switch panel.GetChoice() {
-        case 1:
-            sayHello()
-        case 2:
-            showTime()
-        case 3:
-            doSomethingElse()
-        case 0:
-            fmt.Println(cli.Green + "Goodbye!" + cli.Reset)
-            return
-        default:
-            fmt.Println(cli.Red, "Unknown option. Try again.", cli.Reset)
-        }
+		switch panel.GetChoice() {
+		case 1:
+			sayHello()
+		case 2:
+			showTime()
+		case 3:
+			doSomethingElse()
+		case 0:
+			fmt.Println(cli.Green + "Goodbye!" + cli.Reset)
+			return
+		default:
+			fmt.Println(cli.Red, "Unknown option. Try again.", cli.Reset)
+		}
 
-        fmt.Print("\nPress Enter to continue...")
+		fmt.Print("\nPress Enter to continue...")
 
-        panel.PrintLine()
-    }
+		panel.PrintLine()
+	}
 }
 
 func sayHello() {
-    fmt.Println(cli.Green + "\nHello, world!" + cli.Reset)
+	fmt.Println(cli.Green + "\nHello, world!" + cli.Reset)
 }
 
 func showTime() {
-    now := time.Now().Format("2006-01-02 15:04:05")
-    fmt.Println(cli.Green, "\nCurrent time is", now, cli.Reset)
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println(cli.Green, "\nCurrent time is", now, cli.Reset)
 }
 
 func doSomethingElse() {
-    fmt.Println(cli.Green + "\nDoing something else..." + cli.Reset)
+	fmt.Println(cli.Green + "\nDoing something else..." + cli.Reset)
 }
